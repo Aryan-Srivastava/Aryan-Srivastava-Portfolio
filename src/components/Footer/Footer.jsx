9657976fa82f7873949c05518fc7585eda70514f
Aryan <aryanyash999@gmail.com>
2023-03-30 00:33:17 +0530

    Footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './Footer.modules.css'
+import "./Footer.modules.css";
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer className="footer">
+			<div className="footer__container container social">
+				<div className="footer__socials">
+					<a href="" className="footer__social-link">
+						<FaLinkedin />
+					</a>
+					<a href="" className="footer__social-link">
+						<FaGithub />
+					</a>
+					<a href="" className="footer__social-link">
+						<FaTwitter />
+					</a>
+				</div>
+			</div>
 
-export default Footer
+			<p className="footer__copyright text-cs">
+				&copy; 2023 Developed with ❤️ by{" "}
+				<a href="" className="footer__link">
+					Aryan Srivastava
+				</a>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
