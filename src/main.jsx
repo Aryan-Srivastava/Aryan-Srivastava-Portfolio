0dac6a4c6c98c488bdd73cf49296c0023ed98dec
Aryan <aryanyash999@gmail.com>
2023-05-25 01:41:39 +0530

    Added GA

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
+ReactGA.initialize(TRACKING_ID);
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
