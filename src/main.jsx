be2f49bc236a69de8f796fac80c3516793054e56
Aryan-Srivastava <aryanyash999@gmail.com>
2026-07-06 15:38:42 +0530

    Remove commented-out analytics code from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import ReactGA from "react-ga4";
-import { inject } from "@vercel/analytics";
+// import ReactGA from "react-ga4";
+// import { inject } from "@vercel/analytics";
 
-const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
-inject();
-ReactGA.initialize(TRACKING_ID);
-ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+// const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
+// inject();
+// ReactGA.initialize(TRACKING_ID);
+// ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
